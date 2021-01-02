a7fbff11c0cadcd21017737467cd79342370eb03
Hans Schou <chlor@schou.dk>
2021-01-02 13:10:03 +0100

    Konvertér fra db til asciidoc m/pandoc

---
+++ bin/docbook2asciidoc.bash
@@ -0,0 +1,28 @@
+#! /bin/bash
+# vim: ts=4 :
+set -e
+
+if ! which pandoc > /dev/null
+then
+	echo Fejl, pandoc ikke installere.
+	echo sudo apt install pandoc
+	exit 1
+fi
+
+if [ ! -e bog.sgml ]
+then
+	echo "Fejl, der skal være en 'bog.sgml' fil i dette subdir."
+	exit 2
+fi
+
+if [ ! -d adoc/ ]
+then
+	mkdir adoc
+fi
+
+for S in *.sgml
+do
+	A=adoc/${S/.sgml/}.adoc
+	echo $S \> $A
+	pandoc --wrap=none -f docbook -t asciidoc $S > $A
+done
